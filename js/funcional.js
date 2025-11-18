7e6761e6c92182c307034f2238fe7a494500f651
Vinihaw <vinicius.souza.haw@gmail.com>
2025-11-18 11:40:19 -0300

    Site Provida

---
+++ js/funcional.js
@@ -0,0 +1,8 @@
+                                    //* cabeçalho
+    var myCarousel = document.getElementById('carouselExampleCaptions')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 2000,
+      ride: 'carousel'
+    })
+  
+    
