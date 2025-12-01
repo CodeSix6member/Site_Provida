98ddb096e6c708638288fb34e455c587169f82b7
CodeSix <codesixdev@gmail.com>
2025-12-01 11:24:10 -0300

    funcional.js

--- js/funcional.js
+++ js/funcional.js
@@ -12,10 +12,7 @@
 
       if (menuHamburguer && navList) {
         menuHamburguer.addEventListener('click', () => {
-          // Alterna a classe 'active' que faz o menu aparecer/desaparecer
           navList.classList.toggle('active');
-
-          // Opcional: Animar o ícone (hambúrguer para 'X')
           menuHamburguer.classList.toggle('open');
         });
       }
